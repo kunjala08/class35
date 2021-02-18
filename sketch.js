a37dc23d225a03b0cfe83308d9fefedc32fd79fe
kunjala08 <71957334+kunjala08@users.noreply.github.com>
2021-02-18 20:11:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var ball;
+var database;
+var ballDatabase;
+var position;
+function setup(){
+    
+    database = firebase.database();
+    
+    createCanvas(500,500);
+    ballDatabase = createSprite(250,250,10,10);
+    ballDatabase.shapeColor = "red";
+
+    //set : storing in db == set
+    //get : reading/getting the value from db  ==on
+    var ballDatabasePosition = database.ref('ball/position');
+    ballDatabasePosition.on("value",readPosition,showError);
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+
+
+function showError(){
+    console.log("errors in database")
+}
+
+function readPosition(data){
+    position = data.val();
+    ballDatabase.x = position.x;
+    ballDatabase.y = position.y;
+    
+}
+function writePosition( x, y){
+    database.ref('ball/position').set({
+        'x': position.x + x,
+        'y': position.y + y    
+    })
+    
+}
